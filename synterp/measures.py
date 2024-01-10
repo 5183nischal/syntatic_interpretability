d84a1a75b78cb78a5c87bc9fcd35ef1384ef154f
nisch <nm2508@nyu.edu>
2024-01-10 22:26:46 +0100

    Added notebook and plans for functions to get started

---
+++ synterp/measures.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# TODO: Function to calculate SVD of a low rank matrix.
+# There are better and worse ways for it and generally low rank condition/assumpotion implies we can do this efficiently.
+# I think TransformerLens probably has a good implementation of this.
+
+# TODO: Function to calculate specctral entropy of signular values and calculate participaton ration.
+
+# TODO: Function to keep track of changes in the top singular vectors and approximate the time point when they stablize
+
+# TODO: Function to characcterize thr distribution of the signular values and identify the bluk and the outliers.
+# I expect some long tail power law behavior and we will want to explicitly track the tail.
