f25f2ff0e0f8a47a7a3e3be4f13e6947bc6ab22a
melembroucarlitos <melembroucarlitos@gmail.com>
2024-01-31 23:14:17 +0000

    extracted attn_circuit_error bounds logic into measurements

--- syntactic_interpretability/visualizations.py
+++ syntactic_interpretability/visualizations.py
@@ -1,14 +1,11 @@
 import plotly.graph_objects as go
 import webcolors
-from typing import List, Literal
+from typing import List, Dict
 from dataclasses import dataclass
-import torch
 
-from syntactic_interpretability.metrics.weight_metrics import AttnCircuitMeasurements
+from syntactic_interpretability.metrics.weight_metrics import AttnCircuit, AttnCircuitsMeasurements, Metric
 
-# TODO: Move the cicuit type upper & lower bound logic into the metric class
-# TODO: Once metrics are optional, Depending on circuit, validate all metrics are present
-# TODO: plot_attn_circuit_measurements :: Dict[str, List[AttnCircuitMeasurements]] -> Literal['qk', 'ov'] -> go.Figure
+# TODO: Test plot_attn_circuit_measurements()
 
 @dataclass
 class Line:
@@ -29,35 +26,18 @@ class Line:
             raise ValueError("All lists in Line must have the same length")
     
     @classmethod
-    def from_attn_circuit_measurement(circuits: List[AttnCircuitMeasurements], color: str, circuit_type: Literal['qk', 'ov']) -> "Line":
+    def from_attn_circuit_measurement(circuits: List[AttnCircuitsMeasurements], color: str, circuit_type: AttnCircuit, metric: Metric) -> "Line":
         assert all([circuit.model_name == circuits[0].model_name for circuit in circuits]), "Circuit measurements must come from the same model"
+        assert all([getattr(circuit, circuit_type) is not None for circuit in circuits]), "circuit_type must be present in all AttnCircuitMeasurements"
 
-        def _tensor_min(tensor: torch.Tensor) -> float:
-          flattened_tensor = torch.flatten(tensor)
-          return torch.min(flattened_tensor).item()
-            
-        def _tensor_max(tensor: torch.Tensor) -> float:
-          flattened_tensor = torch.flatten(tensor)
-          return torch.max(flattened_tensor).item()
-
-        if circuit_type == 'qk':
-          ys = [circuit.qk_reduced for circuit in circuits]
-          lower_bound = [_tensor_min(circuit.qk) for circuit in circuits]
-          upper_bound = [_tensor_max(circuit.qk) for circuit in circuits]
-        elif circuit_type == 'ov':
-          ys = [circuit.ov_reduced for circuit in circuits]
-          lower_bound = [_tensor_min(circuit.ov) for circuit in circuits]
-          upper_bound = [_tensor_max(circuit.ov) for circuit in circuits]
-        else:
-           raise ValueError(f"circuit type must be either 'qk' or 'ov': {circuit_type}")
-        
+        index = f'{circuit_type}.{metric}'
         return Line(
             name=circuits[0].model_name,
             color=color,
             x=[circuit.num_tokens_seen for circuit in circuits],
-            y=ys,
-            lower_bound=lower_bound,
-            upper_bound=upper_bound
+            y=[circuit.mean_reducer(index) for circuit in circuits],
+            lower_bound=[circuit.tensor_min(index) for circuit in circuits],
+            upper_bound=[circuit.tensor_max(index) for circuit in circuits]
         )
     
 
@@ -65,7 +45,7 @@ def continuous_error_boundary_plot(
     lines: List[Line], 
     title: str,
     x_axis_title: str,
-    y_axis_title: str
+    y_axis_title: str,
 ) -> go.Figure:
     fig = go.Figure()
     for line in lines:
@@ -89,6 +69,17 @@ def continuous_error_boundary_plot(
     
     return fig
 
+def plot_attn_circuit_measurements(
+    measurements_color_dict: Dict[str, List[AttnCircuitsMeasurements]], 
+    circuit_type: AttnCircuit, 
+    metric: Metric,
+    title: str,
+    x_axis_title: str,
+    y_axis_title: str
+) -> go.Figure:
+  lines = [Line.from_attn_circuit_measurement(measurements, color, circuit_type, metric) for color, measurements in measurements_color_dict.items()]
+  return continuous_error_boundary_plot(lines=lines, title=title, x_axis_title=x_axis_title, y_axis_title=y_axis_title)
+        
 if __name__ == "__main__":
   import numpy as np
   lines = [
